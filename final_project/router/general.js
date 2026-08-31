931eb754b188d5dcc38a136b8d71df2c5ffa709e
Sandhya828386 <sravanasandhya2021@gmail.com>
2026-08-31 15:24:24 +0530

    Complete Q4 books by author
    Implemented functionality to retrieve books by author.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -37,9 +37,17 @@ public_users.get('/isbn/:isbn', function (req, res) {
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  //Write your code here
+  const author = req.params.author;
 
-  return res.status(300).json({ message: 'Yet to be implemented' });
+  const result = Object.values(books).filter(
+    (book) => book.author.toLowerCase() === author.toLowerCase()
+  );
+
+  if (result.length > 0) {
+    return res.json(result);
+  }
+
+  return res.status(404).json({ message: 'No books found for this author' });
 });
 
 
