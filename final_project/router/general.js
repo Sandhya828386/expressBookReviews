8759a4ed5639b03a3d37d2cb59279568311ef250
Sandhya828386 <sravanasandhya2021@gmail.com>
2026-08-31 06:27:10 -0400

    Complete Q7 user registration

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,6 @@ const express = require('express');
 let books = require('./booksdb.js');
 
 let isValid = require('./auth_users.js').isValid;
-
 let users = require('./auth_users.js').users;
 
 const public_users = express.Router();
@@ -11,7 +10,28 @@ const public_users = express.Router();
 
 // Register a new user
 public_users.post('/register', (req, res) => {
-  return res.status(300).json({ message: 'Yet to be implemented' });
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({
+      message: 'Username and password are required'
+    });
+  }
+
+  if (isValid(username)) {
+    return res.status(400).json({
+      message: 'User already exists'
+    });
+  }
+
+  users.push({
+    username: username,
+    password: password
+  });
+
+  return res.status(201).json({
+    message: 'User registered successfully'
+  });
 });
 
 
@@ -29,7 +49,9 @@ public_users.get('/isbn/:isbn', function (req, res) {
     return res.json(books[isbn]);
   }
 
-  return res.status(404).json({ message: 'Book not found' });
+  return res.status(404).json({
+    message: 'Book not found'
+  });
 });
 
 
@@ -77,7 +99,9 @@ public_users.get('/review/:isbn', function (req, res) {
     return res.json(books[isbn].reviews);
   }
 
-  return res.status(404).json({ message: 'Book not found' });
+  return res.status(404).json({
+    message: 'Book not found'
+  });
 });
 
 
